42ee4250346242b08b1bb5e5e3cb44767d07e606
Ankhit <tankhit535@gmail.com>
2022-03-11 17:39:28 +0100

    Initial commit

---
+++ src/main/java/Calc.java
@@ -0,0 +1,14 @@
+public class Calc {
+    public int add(int a,int b){
+        return(a+b);
+    }
+
+    public int sub(int a, int b) {
+        return a-b;
+    }
+
+    public int div(int a, int b) { return a/b; }
+
+    public int mult(int a, int b) {return a*b; }
+
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,18 @@
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+    public class Tests {
+        public static Calc calc;
+        @BeforeClass
+        public static void setup(){
+            calc = new Calc();
+        }
+
+        @Test
+        public void testAdd(){
+            assertEquals(5,calc.add(2,3));
+        }
+}
