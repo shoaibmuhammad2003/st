3103418bc0ca8464600b618ad5bb46a7f4af02c0
Y-Bhavya <70648992+Y-Bhavya@users.noreply.github.com>
2022-03-12 17:29:21 +0530

    Test Case failed by bhavya yaddula

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -26,7 +26,7 @@ import static org.junit.Assert.assertEquals;
         @Test
         //Test case by Bhavya Yaddula
         public void testDiv() {
-            assertEquals(1, calc.div(9, 9));
+            assertEquals(1, calc.div(9, 8));
         }
 
     }
